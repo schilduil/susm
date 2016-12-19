0083c676031ccc78a881ea025c233f7140be8802
Bert Raeymaekers <bert.raeymaekers@schilduil.org>
2016-12-19 21:40:15 +0100

    Make start also callable from other locations.

--- start.sh
+++ start.sh
@@ -8,5 +8,6 @@ SCRIPT=$(readlink -f "$0")
 # Path of the current script
 BASEDIR=$(dirname "$SCRIPT")
 
+cd ${BASEDIR}
 python3 -m suapp -c "$BASEDIR/susm.json" -t localweb -o "log.level=$LOGLEVEL"
 
